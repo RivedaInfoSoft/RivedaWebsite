13ece80a879de6b4287c2a2f0336448c569fea12
RivedaInfoSoft <varadharajuk@rivedainfosoft.com>
2025-06-09 17:49:13 +0530

    Add reusable GA snippet

---
+++ js/ga.js
@@ -0,0 +1,11 @@
+(function(){
+  var script=document.createElement('script');
+  script.async=true;
+  script.src='https://www.googletagmanager.com/gtag/js?id=G-0000000000';
+  document.head.appendChild(script);
+
+  window.dataLayer=window.dataLayer||[];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-0000000000');
+})();
